a4969844c09d36c5417c0524b09c07e2118febaa
K1018112 <K1018112@A50020562.kuds.kingston.ac.uk>
2013-12-11 17:25:42 +0000

    new commit

---
+++ src/com/toy/anagrams/lib/Noob.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toy.anagrams.lib;
+
+/**
+ *
+ * @author K1018112
+ */
+public class Noob {
+    
+}
